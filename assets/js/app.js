23c4ac655e8e4602ae291a1e322b2ddb441553b0
Ednas <edna@edna.tech>
2018-08-01 00:15:28 -0400

    Added a random number fnc and add comments to css and code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
+function randonum() {
+    let rando = Math.floor(Math.random() * 5) + 1;
+    console.log(rando);
+    return rando;
+};
 //Hardcoded ratings since I'm not setting up a database for this
 const ratings = {
-    edna: 2.8,
-    brandy: 3.3,
-    mac: 1.9,
-    davie: 4.3,
-    grendel: 4.74
+    edna: randonum(),
+    brandy: randonum(),
+    mac: randonum(),
+    davie: randonum(),
+    grendel: randonum()
 };
 
 const starTotal = 5;
@@ -17,6 +22,7 @@ for (const rating in ratings) {
     const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
     // for the rating selects the stars class and sets the width to the Star percentage rounded
     document.querySelector(`.${rating} .stars`).style.width = starPercentageRounded;
+    // This will add the whole star ontop of the star outline and fill it in
     $('.stars').toggleClass('fas fa-star');
     $('.stars').append('<i class="fas fa-star"></i>')
 
